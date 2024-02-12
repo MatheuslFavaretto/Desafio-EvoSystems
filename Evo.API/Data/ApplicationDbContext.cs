b2785e978b0b6f5afea1f2f0a1dc208579cd41c1
Matheus Favaretto <116848225+MatheuslFavaretto@users.noreply.github.com>
2024-02-12 06:59:42 -0300

    Update ApplicationDbContext.cs
    removido duplicata da função

--- Evo.API/Data/ApplicationDbContext.cs
+++ Evo.API/Data/ApplicationDbContext.cs
@@ -7,10 +7,6 @@ namespace Evo.API.Data
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext()
-        {
-        }
-
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
 
         public DbSet<Department> Departments { get; set; }
